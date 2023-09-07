98ddeeea88e43f9cf737244570094e54760b9d1b
NJesus144 <nalbert-edm@outlook.com>
2023-09-07 12:31:28 -0300

    criado rota snacks e adicionado schemas no prisma

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 
@@ -5,11 +7,28 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "hello world!" });
+});
+
+app.get("/snacks", async (req: Request, res: Response) => {
+  const { snack } = req.query;
+
+  if (!snack) return res.status(400).send({ error: "Snack is required" });
+
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  });
+
+  res.send(snacks)
 });
 
 app.listen(port, () => {
